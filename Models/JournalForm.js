9a99ff18f9285963dc261e51bf1de62ff190b362
vilgeralorraine <vilgeralorraine@gmail.com>
2025-01-15 23:36:29 +0800

    feat: :bug: try fixing array

--- Models/JournalForm.js
+++ Models/JournalForm.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const FormSchema = new mongoose.Schema({
-    name: { type: [String], required: true },
-    message: { type: [String], required: true },
+    name: { type: String, required: true },
+    message: { type: String, required: true },
 });
 
 // const JournalForm = mongoose.model("JournalForm", FormSchema);
